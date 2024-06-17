b6a8a277eec4d9bbe3bbef67f512b1f7cbd1bf5d
pli <chordplaylist@gmail.com>
2024-06-17 17:42:40 +0900

    init: init project

---
+++ src/main/java/com/pli/springmultidatasourceex/SpringMultiDatasourceExApplication.java
@@ -0,0 +1,13 @@
+package com.pli.springmultidatasourceex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultiDatasourceExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultiDatasourceExApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pli/springmultidatasourceex/SpringMultiDatasourceExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pli.springmultidatasourceex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultiDatasourceExApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
